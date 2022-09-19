5e71ea80ca3baab65bbb5b31191eff49d8ef9cf3
Katlyn McDonald <kkmcoding@gmail.com>
2022-09-19 04:39:12 -0400

    video 14.3.3 done

--- Desktop/coursework/Coding Events/src/main/java/org/launchcode/codingevents/models/Event.java
+++ Desktop/coursework/Coding Events/src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,17 +1,25 @@
 package org.launchcode.codingevents.models;
 
+import java.util.Objects;
+
 public class Event {
 
     private String name;
     private String description;
+    private int id;
+    private static int nextId = 1;
 
     public Event(String name, String description) {
         this.name = name;
         this.description = description;
+        this.id = nextId;
+        nextId++;
     }
 
     public Event(String name) {
         this.name = name;
+        this.id = nextId;
+        nextId++;
     }
 
     public String getName() {
@@ -30,8 +38,26 @@ public class Event {
         this.description = description;
     }
 
+    public int getId() {
+        return id;
+    }
+
     @Override
     public String toString() {
         return name;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Event event = (Event) o;
+        return id == event.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
 }
